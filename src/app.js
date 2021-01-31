ccdba07294c66c9c7a01e210fb53edd161aa883a
JJ Quispe <joelquispeunju@gmail.com>
2021-01-30 22:55:05 -0300

    Completed tasks:
    - delete cors
    - add hello endpoint

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express, { json } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cors from "cors";
+import dotenv from "dotenv";
+import { HttpStatusCode } from "./utils";
 
 const app = express();
+dotenv.config();
 
-app.use(cors());
 app.use(helmet());
 app.use(json());
 app.use(morgan("dev"));
+
+app.get("*", (req, res) => {
+    res.status(HttpStatusCode.OK).send("Welcome to the our API Rest");
+});
 
 export default app;
---
+++ src/utils/index.js
@@ -0,0 +1,4 @@
+import HttpStatusCode from "./status_code";
+import HttpStatusMessage from "./status_message";
+
+export { HttpStatusCode, HttpStatusMessage };
